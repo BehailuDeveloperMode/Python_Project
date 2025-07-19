5d4f23cc27458beac8b10e33453a59239b64a2ba
Behailu Tessema <132300876+BehailuDeveloperMode@users.noreply.github.com>
2025-07-19 02:33:36 -0700

    Create coverage_policy_FK.sql

---
+++ YAYOBE_Auto_Insurance/YAYOBE_Auto_Insurance_Relational/SQL_Server_2012/DataObjects/ForeignKey/coverage_policy_FK.sql
@@ -0,0 +1,12 @@
+ALTER TABLE coverage 
+    ADD CONSTRAINT coverage_policy_FK FOREIGN KEY 
+    ( 
+     policy_policy_id
+    ) 
+    REFERENCES policy 
+    ( 
+     policy_id 
+    ) 
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION 
+GO
